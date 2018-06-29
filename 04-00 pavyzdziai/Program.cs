050d81d743d46678370f686f3929a8a8da7195ef
Ieva <ieva.jaksaityte@gmail.com>
2018-06-29 17:59:01 +0300

    atnaujintas pavyzdys

--- 04-00 pavyzdziai/Program.cs
+++ 04-00 pavyzdziai/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -38,17 +39,18 @@ namespace _04_00_pavyzdziai
             Console.WriteLine("PIN kodas?");
             var pin = Convert.ToInt32(Console.ReadLine());
 
-            // gimimo data
-            // lyti
+            Console.WriteLine("Gimimo data?");
+            var gimimoData = Console.ReadLine();
+
+            Console.WriteLine("Lytis?");
+            var lytis = Console.ReadLine();
 
             Console.WriteLine("Jusu suvesti duomenys:");
-
-            // patys sugalvokite isvedima, pvz:
-
-            // {vardas} {pavarde} ({amzius} m.)
-            // Jusu vairuojamas auto: ...
-            // Jusu megstamiausia spalva: ...
-            // Nebe jusu korteles duomenys: ...
+            Console.WriteLine(vardas + " " + pavarde + " vairuoja " + auto);
+            Console.WriteLine("Asmuo gimes " + gimimoData + ", jo amzius " + amzius + ", yra " + lytis + ", jo ugis " + ugis + " m.");
+            Console.WriteLine("Siuo metu gyvena " + vieta);
+            Console.WriteLine("Megstamiausia spalva " + spalva);
+            Console.WriteLine("Korteles numeris " + kortelesNr + ", kurios pin kodas " + pin);
 
             /*
              suvesti šią informaciją apie gyvūną to klausinėjant
@@ -61,15 +63,42 @@ namespace _04_00_pavyzdziai
              šią informaciją išvesti po visų įvedimų kaip nors gražiai
              */
 
-            Console.WriteLine("Gyvūno vardas?");
+            Console.Write("Gyvūno vardas: ");
             var gyvunoVardas = Console.ReadLine();
 
-            // ...
+            Console.Write("Kailio spalva: ");
+            var kailis = Console.ReadLine();
+
+            Console.Write("Kilme: ");
+            var kilme = Console.ReadLine();
+
+            Console.Write("Rusis: ");
+            var rusis = Console.ReadLine();
+
+            Console.Write("Greitis km/h: ");
+            var greitis = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("GYVUNO DUOMENYS");
+            Console.WriteLine("Vardas: " + gyvunoVardas);
+            Console.WriteLine("Kailio spalva: " + kailis);
+            Console.WriteLine("Kilme: " + kilme);
+            Console.WriteLine("Rusis: " + rusis);
+            Console.WriteLine("Greitis: " + greitis + " km/h");
 
             /* liepti vartotojui suvesti 5 skaičius vieną po kito
              * išvesti į ekraną pirmą ir paskutinį skaičius
              * išvesti į ekraną antro, trečio ir ketvirto skaičių sumą
              */
+
+            Console.WriteLine("Suveskite 5 skaicius, po kiekvieno spausdami enter mygtuka");
+            var pirmas = Convert.ToInt32(Console.ReadLine());
+            var antras = Convert.ToInt32(Console.ReadLine());
+            var trecias = Convert.ToInt32(Console.ReadLine());
+            var ketvirtas = Convert.ToInt32(Console.ReadLine());
+            var penktas = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(pirmas + " " + penktas);
+            Console.WriteLine(antras * trecias * ketvirtas);
         }
     }
 }
