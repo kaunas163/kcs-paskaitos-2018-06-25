b9a5def168395b8f842738e5e72626a80d11b126
Ieva <ieva.jaksaityte@gmail.com>
2018-07-02 00:07:21 +0300

    uzduotys

---
+++ 04-07 uzduotis/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04_07_uzduotis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+                Parašykite programą, kuri laipsnius paverstų į kelvinus ir fahrenheitus.
+             */
+
+            Console.WriteLine("Iveskite laipsnius");
+            var laipsniai = Convert.ToInt32(Console.ReadLine());
+            var kelvinai = laipsniai + 273.15;
+            var fahrenheitai = (double)laipsniai * 9 / 5 + 32;
+            Console.WriteLine("{0} laipsniu yra {1} kelvinais", laipsniai, kelvinai);
+            Console.WriteLine("{0} laipsniu yra {1} fahrenheitais", laipsniai, fahrenheitai);
+        }
+    }
+}
---
+++ 04-08 uzduotis/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04_08_uzduotis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+                 Liepkite įvesti tris skaičius. Išveskite įvestus
+                 skaičius, jų kvadratus, juos pakeltus trečiuoju
+                 laipsniu, tą skaičių padalintą iš dviejų.
+             */
+
+            Console.WriteLine("Iveskite tris skaicius");
+            var pirmas = Convert.ToInt32(Console.ReadLine());
+            var antras = Convert.ToInt32(Console.ReadLine());
+            var trecias = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("{0} {1} {2} {3}", pirmas, pirmas * pirmas, Math.Pow(pirmas, 3), (double)pirmas / 2);
+            Console.WriteLine("{0} {1} {2} {3}", antras, antras * antras, Math.Pow(antras, 3), (double)antras / 2);
+            Console.WriteLine("{0} {1} {2} {3}", trecias, trecias * trecias, Math.Pow(trecias, 3), (double)trecias / 2);
+        }
+    }
+}
