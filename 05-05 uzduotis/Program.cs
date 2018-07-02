2fdcfc152109611189bf458cdfd0d6eb4992ec96
Ieva <ieva.jaksaityte@gmail.com>
2018-07-02 23:51:05 +0300

    if uzduotys

--- 05-05 uzduotis/Program.cs
+++ 05-05 uzduotis/Program.cs
@@ -21,7 +21,36 @@ namespace _05_05_uzduotis
                   sumą ir sandaugą.
              */
 
+            Console.WriteLine("Iveskite skaiciu");
+            var skaicius = Convert.ToInt32(Console.ReadLine());
 
+            // ar skaičius dalinasi iš 5?
+
+            if (skaicius % 5 == 0)
+            {
+                Console.WriteLine("{0} * 1 = {1}", skaicius, skaicius * 1);
+                Console.WriteLine("{0} * 2 = {1}", skaicius, skaicius * 2);
+                Console.WriteLine("{0} * 3 = {1}", skaicius, skaicius * 3);
+                Console.WriteLine("{0} * 4 = {1}", skaicius, skaicius * 4);
+                Console.WriteLine("{0} * 5 = {1}", skaicius, skaicius * 5);
+            }
+
+            // ar skaičius lyginis?
+
+            if (skaicius % 2 == 0)
+            {
+                Console.WriteLine("skaiciaus {0} penktas laipsnis {1}", skaicius, Math.Pow(skaicius, 5));
+            }
+
+            // ar skaičius nesidalina iš 4?
+
+            if (skaicius % 4 != 0)
+            {
+                Console.WriteLine("Iveskite skaiciu");
+                var antras = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("{0} + {1} = {2}", skaicius, antras, skaicius + antras);
+                Console.WriteLine("{0} * {1} = {2}", skaicius, antras, skaicius * antras);
+            }
         }
     }
 }
