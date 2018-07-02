5855dfe3f6c9ddfec5d17cae04449968fc7769f1
Ieva <ieva.jaksaityte@gmail.com>
2018-07-03 00:13:44 +0300

    else uzduotys

---
+++ 07-01 uzduotis/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _07_01_uzduotis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+                Liepkite vartotojui įvesti du skaičius. Raskite
+                kuris skaičius yra didesnis. Su viena if
+                sąlyga (bei visomis jos dalimis) atlikite šiuos patikrinimus:
+                - ar pirmas skaičius didesnis už antrą skaičių;
+                - ar antras skaičius didesnis už pirmą;
+                - jei niekas neatitinka - tuomet skaičiai lygūs.
+             */
+
+            Console.WriteLine("Iveskite du skaicius");
+            var pirmas = Convert.ToInt32(Console.ReadLine());
+            var antras = Convert.ToInt32(Console.ReadLine());
+
+            if (pirmas > antras)
+            {
+                Console.WriteLine("pirmas didesnis");
+            }
+            else if (antras > pirmas)
+            {
+                Console.WriteLine("antras didesnis");
+            }
+            else
+            {
+                Console.WriteLine("skaiciai lygus");
+            }
+        }
+    }
+}
---
+++ 07-02 uzduotis/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _07_02_uzduotis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+                Liepkite vartotojui įvesti vieną norimą skaičių. Su
+                viena if sąlyga (ir visomis jos dalimis),
+                patikrinkite šiuos dalykus:
+                - ar skaičius neigiamas;
+                - ar skaičius teigiamas;
+                - ar skaičius lygus 0.
+             */
+
+            Console.WriteLine("Iveskite skaiciu");
+            var skaicius = Convert.ToInt32(Console.ReadLine());
+
+            if (skaicius < 0)
+            {
+                Console.WriteLine("skaicius neigiamas");
+            }
+            else if (skaicius > 0)
+            {
+                Console.WriteLine("skaicius teigiamas");
+            }
+            else
+            {
+                Console.WriteLine("skaicius lygus 0");
+            }
+        }
+    }
+}
