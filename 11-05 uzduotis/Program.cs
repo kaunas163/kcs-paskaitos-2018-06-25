3bf27acb4dbdfb7b783f63e9a5e3dec8641b8413
Ieva <ieva.jaksaityte@gmail.com>
2018-07-03 22:55:08 +0300

    for ciklo uzduotys

---
+++ 11-05 uzduotis/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _11_05_uzduotis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+                Liepkite vartotojui įvesti rėžių pradžią ir pabaigą. Patikrinkite, 
+                kad tai būtų validu (pradžia turi būti mažesnė nei pabaiga). Jei rėžiai 
+                tinkami, tuomet vykdyti for, kuris atskirose eilutėse išvestų kiekvieną 
+                skaičių iš tų rėžių, bei atskiriant tarpu - tų skaičių kvadratus.
+             */
+
+            Console.Write("Iveskite reziu pradzia: ");
+            var pradzia = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Iveskite reziu pabaiga: ");
+            var pabaiga = Convert.ToInt32(Console.ReadLine());
+
+            if (pradzia < pabaiga)
+            {
+                for (int i = pradzia; i <= pabaiga; i++)
+                {
+                    Console.WriteLine(i + " " + i * i);
+                }
+            }
+        }
+    }
+}
---
+++ 11-06 uzduotis/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _11_06_uzduotis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+                Liepkite vartotojui įvesti rėžių pradžią ir pabaigą. Patikrinkite, 
+                kad tai būtų validu (pradžia turi būti mažesnė nei pabaiga). Jei rėžiai 
+                tinkami, tuomet vykdyti for, kuris iš duotų skaičių išvestų visus 
+                nelyginius skaičius arba tuos, kurie dalinasi iš 8.
+             */
+
+            Console.Write("Iveskite reziu pradzia: ");
+            var pradzia = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Iveskite reziu pabaiga: ");
+            var pabaiga = Convert.ToInt32(Console.ReadLine());
+
+            if (pradzia < pabaiga)
+            {
+                for (int i = pradzia; i <= pabaiga; i++)
+                {
+                    if (i % 2 != 0 || i % 8 == 0)
+                    {
+                        Console.WriteLine(i);
+                    }
+                }
+            }
+        }
+    }
+}
